6301a93a6682fc7fcdef98089795f1d702e95628
Alex Gaspar <al.gaspar@gmail.com>
2017-07-04 19:16:36 +0300

    Use vexflow to render notes

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,56 +1,20 @@
-$(function() {
-  var socket = io('http://192.168.0.8:3000');
+"use strict";
 
-  var RANGE_MIN = 3;
-  var RANGE_MAX = 6;
+function NoteGenerator (options) {
+  options = options || {};
+  let RANGE_MIN = options.min || 3;
+  let RANGE_MAX = options.max || 6;
 
-  var NOTES = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-  var NOTES_SHARP = ['C', 'D', 'F', 'G', 'A'];
+  const NOTES = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  const NOTES_SHARP = ['C', 'D', 'F', 'G', 'A'];
 
-  var CURRENT_NOTE = {};
-  var WAIT_BETWEEN_NOTES = 300; // in ms
-
-  var getRandomArbitrary = function (min, max) {
+  this.getRandomArbitrary = function (min, max) {
     return Math.round(Math.random() * (max - min) + min);
   };
 
-  var isOutofLine = function (data) {
-    if( data.range === 5 && data.note === 'C') {
-      return true;
-    }
-  };
-
-  var $createNote = function(data) {
-    return $('<span id="note" class="current-note round ' + keyNumber(data) + '"></span>')
-  };
-
-  var keyNumber = function (data) {
-    return data.note + data.range;
-  };
-
-  var showNote = function (data, color, classes) {
-    var $note = $createNote(data, color);
-    $note.css('background-color', color);
-    $note.addClass(classes);
-
-    if (data.type === '#') {
-      $note.addClass('sharp');
-    }
-
-    if ( isOutofLine(data) ) {
-      $note.addClass('extra-line');
-    }
-
-    $('.sheet-wrapper').append($note);
-  };
-
-  var clearAllNotes = function () {
-    $("#note").remove();
-  };
-
   // F3 -- G6
-  window.generateRandomNote = function () {
-    var range = getRandomArbitrary(RANGE_MIN, RANGE_MAX);
+  this.getNote = function () {
+    var range = this.getRandomArbitrary(RANGE_MIN, RANGE_MAX);
     var min = 0, max = 6;
 
     if (range == 3) {
@@ -59,44 +23,165 @@ $(function() {
       max = 5;
     }
 
-    var note = NOTES[ getRandomArbitrary(min, max) ];
+    var note = NOTES[ this.getRandomArbitrary(min, max) ];
 
-    CURRENT_NOTE = {
-      note: note,
-      type: getRandomArbitrary(1, 5) % 4 == 0 && _.includes(NOTES_SHARP, note) ? '#' : null,
-      range: range
-    };
-
-    console.log(CURRENT_NOTE);
-    showNote(CURRENT_NOTE, '#000');
+    return note + (this.getRandomArbitrary(1, 5) % 4 == 0 && _.includes(NOTES_SHARP, note) ? '#' : '') + range;
   };
 
-  socket.on('note unpressed', function (data) {
-    var selector = '.' + keyNumber(data);
-    if (data.type === '#') {
-      selector += '.sharp';
+  return this;
+}
+
+const NOTES_1 = 'C#5/q, B4, A4, G#4, C#5/q, B4, A4, G#4, C#5/q, B4, A4, G#4, C#5/q, B4, A4, G#4';
+const NOTES_2 = 'E5/q, D#5, E5, D#5, E5, B4, D5, C5, A4'
+
+function Sheet (options) {
+  options = options || {};
+
+  let width = options.width || 1400;
+  let height = options.height || 1400;
+  let selector = options.selector || 'sheet';
+
+  let currentNoteIndex = 0;
+  let sheetNotes = options.notes || 'C#5/q';
+  let sheetNotesArray = sheetNotes.split(',').map((note) => note.split('/')[0].trim());
+
+  let playedNotes = [];
+
+  let noteGenerator = new NoteGenerator();
+
+  // Make VexFlow global
+  let vf, vfX, vfY;
+
+  this.resetSVG = function () {
+    // Clear current html container
+    $('#' + selector).html('');
+
+    // reset draw variables
+    vfY = 10;
+    vfX = 10;
+  };
+
+  this.formatCurrentPlayedNote = (playedNoteString) => {
+    if (playedNotes.length == 0) {
+      return playedNoteString + '/q';
     } else {
-      selector += ':not(.sharp)';
+      return playedNotes.join(',') + ', ' + playedNoteString;
     }
+  };
 
-    console.log(selector);
+  this.addPlayedNote = (playedNoteString) => {
+    if (playedNotes.length === 0) {
+      playedNotes.push(playedNoteString + '/q');
+    } else {
+      playedNotes.push(playedNoteString);
+    }
+  };
 
-    $(selector).remove();
+  this.shouldAdvance = (playedNoteString, strictMode) => {
+    if (strictMode) {
+      this.addPlayedNote(playedNoteString);
+      if (playedNoteString === sheetNotesArray[currentNoteIndex]) {
+        currentNoteIndex++;
+      }
+    } else {
+      if (playedNoteString === sheetNotesArray[currentNoteIndex]) {
+        currentNoteIndex++;
+        this.addPlayedNote(playedNoteString);
+      }
+    }
+  };
+
+  this.tick = (playedNote, options) => {
+    options = options || {strick: true};
+    playedNote = playedNote.trim().toUpperCase();
+
+    this.draw(playedNote);
+    this.shouldAdvance(playedNote, options.strick);
+  };
+
+
+  this.makeSystem = function (width) {
+    let system = vf.System({ x: vfX, y: vfY, width: width, spaceBetweenStaves: 10 });
+    vfX += width;
+    return system;
+  };
+
+  this.drawSystem = (notes) => {
+    let score = vf.EasyScore();
+    let notesChunck = _.chunk(notes, 4);
+
+    notesChunck.forEach((notesGroup, index) => {
+      let sys = this.makeSystem(250).addStave({
+        voices: [
+          score.voice(notesGroup).setStrict(false)
+        ]
+      });
+
+      // Start of line
+      if (index == 0 || index % 4 == 0) sys.addClef('treble').addTimeSignature('4/4');
+      // End of line
+      if (index != 0 && (index + 1) % 4 == 0 && index != (notesChunck.length - 1)) this.vfNewLine();
+    });
+
+    this.vfNewLine();
+  };
+
+  this.vfNewLine = function () {
+    vfY += 150;
+    vfX = 10;
+  };
+
+  this.draw = (playedNoteString) => {
+    // Clear current SVG
+    this.resetSVG();
+
+    vf = new Vex.Flow.Factory({
+      renderer: {selector: selector, width: width, height: height}
+    });
+
+    let score = vf.EasyScore();
+
+    // What I should play
+    let notes = score.notes(sheetNotes, {stem: 'up'});
+    this.drawSystem(notes);
+
+    // What I've played
+    let playedNote = score.notes(this.formatCurrentPlayedNote(playedNoteString), {stem: 'up'});
+    this.drawSystem(playedNote);
+
+    vf.draw();
+  };
+
+  this.test = () => {
+    this.tick(noteGenerator.getNote());
+  };
+
+  // Start the game
+  this.tick('');
+  playedNotes = [];
+
+  return this;
+}
+
+
+function initVexFlow () {
+  window.a = new Sheet({
+    notes: NOTES_2
   });
+}
+
+
+$(function() {
+  // Init VexFlow
+  initVexFlow();
+
+  var socket = io('http://0.0.0.0:3000');
+
+  // socket.on('note unpressed', function (data) {
+  // });
 
   socket.on('note pressed', function(data) {
     console.log(data);
-    console.log(CURRENT_NOTE);
-
-    if (_.isEqual(data, CURRENT_NOTE)) {
-      showNote(data, '#00FF00', 'correct');
-      clearAllNotes();
-
-      setTimeout(generateRandomNote, WAIT_BETWEEN_NOTES);
-    } else {
-      showNote(data, '#FF0000', 'playing');
-    }
+    a.tick(data.note);
   });
-
-  generateRandomNote();
 });
